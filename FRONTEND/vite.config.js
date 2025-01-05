613530dafd93cd500b32b4c269bda73150f5f3e4
Shafiya Begum <shafiya_begum@srmap.edu.in>
2025-01-05 21:34:17 +0530

    DEPLOYING FRONTEND

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -4,6 +4,9 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // This sets the output directory to 'build'
+  },
   plugins: [react()],
   resolve: {
     alias: {
